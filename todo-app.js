f16dfc4d2026342e483bfb534fb0abbcb4167cee
zamindar-786 <zamindar900@gmail.com>
2020-06-23 15:24:48 +0530

    checked the task elemenet need to added for react-todo

--- todo-app.js
+++ todo-app.js
@@ -5,14 +5,14 @@ function Task(props) {
 class TodoList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {list: props.list};
+        this.state = { list: props.list };
 
         this.handleAddTask = this.handleAddTask.bind(this);
     }
     handleAddTask(task) {
         console.log("add task clicked");
         this.state.list.push(task);
-        this.setState({list: this.state.list})
+        this.setState({ list: this.state.list })
     }
     render() {
         return (
@@ -33,7 +33,7 @@ class TodoList extends React.Component {
 class TaskNameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = { value: '' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -43,22 +43,24 @@ class TaskNameForm extends React.Component {
         const taskList = this.props.taskList;
         // create a task object
         event.preventDefault();
-        const task = {id:Date.now(), name: this.state.value, 
-        dueDate: new Date()};
+        const task = {
+            id: Date.now(), name: this.state.value,
+            dueDate: new Date()
+        };
         // add the task object to the task list
         this.props.onAddTask(task);
     }
 
     handleChange(event) {
         // code to set the state of the component
-        this.setState({value: event.target.value});
+        this.setState({ value: event.target.value });
     }
 
     render() {
-        return(
+        return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" value={this.state.value} 
-                onChange={this.handleChange}/>
+                <input type="text" value={this.state.value}
+                    onChange={this.handleChange} />
                 <input type="submit" value="Add Task" />
             </form>
         );
